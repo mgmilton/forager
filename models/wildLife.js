aef01c2b80a9f14ce6a2ab4a3c1cf5b94e04911e
mgmilton <matt.milton@gmail.com>
2018-05-30 20:56:18 -0600

    adds index

--- models/wildLife.js
+++ models/wildLife.js
@@ -3,6 +3,14 @@ const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
 class WildLife {
+  static all(){
+    return database('wild_life').select('*')
+  }
+
+  static find(id){
+    return database('wild_life').where('id', id)
+  }
+  
   static create(attributes){
     return database('wild_life').insert(attributes).returning('*')
   }
