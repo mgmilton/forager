9c3787b4c95c2df00f8d99c85af5ca61ee5e1826
mgmilton <matt.milton@gmail.com>
2018-05-30 20:19:22 -0600

    adds wildelife get and post routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var wildLife = require('../models/wildLife.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/wildlife', function(req, res){
+  var query = wildLife.find({});
+  query.exec(function(err, wildlife){
+    if(err){
+      res.send(err);
+    } else{
+      res.json(wildlife);
+    }
+  })
+});
+
+
+router.post('/wildlife', function(req, res){
+  var newWildLife = new wildLife(req.body);
+
+  newWildLife.save(function(err){
+    if(err){
+      res.send(err);
+    } else{
+      res.json(req.body);
+    }
+  })
+}
+)
 
 module.exports = router;
