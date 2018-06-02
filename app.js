98b957574af1ae9fabac327f682ba569147cc7f4
mgmilton <matt.milton@gmail.com>
2018-06-02 12:57:22 -0600

    adds submission router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var wildLifeRouter = require('./routes/wildlife');
+var submitRouter = require('./routes/submit');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/wildlife', wildLifeRouter);
+app.use('/submit', submitRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
