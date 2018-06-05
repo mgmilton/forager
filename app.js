9c0e4e7a24c216d6eadd6f7ad7c07037c9d9e7cc
mgmilton <matt.milton@gmail.com>
2018-06-05 14:29:19 -0600

    deployment

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.bodyencoded({ extended: true }));
+app.use(express.bodyencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
